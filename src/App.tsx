d42263cd615554f3c31146d2ea03d1ef0e022cdf
afrontend <autoscripts@gmail.com>
2025-08-18 20:55:32 +0900

    build with vite instead of react-scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import * as keyboard from 'keyboard-handler';
-import React, { Component } from 'react';
-import _ from 'lodash';
-import './App.css';
-import fpBlock from 'fp-block';
+import * as keyboard from "keyboard-handler";
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import _ from "lodash";
+import "./App.css";
+import fpBlock from "fp-block";
 
 interface Block {
   color: string;
@@ -36,86 +36,98 @@ const DOWN = 40;
 const KEY_L = 76;
 const KEY_S = 83;
 
-const createBlocks = (ary: Block[]): React.ReactElement[] => (
-  ary.map(
-    (item, index) => (
-      <BlockComponent color={item.color} key={index}>
-        {item.count}
-      </BlockComponent>
-    )
-  )
-);
+const createBlocks = (ary: Block[]): React.ReactElement[] =>
+  ary.map((item, index) => (
+    <BlockComponent color={item.color} key={index}>
+      {item.count}
+    </BlockComponent>
+  ));
 
 const blockClassName = (props: BlockProps): string => {
-  return 'block ' + (props.color === 'yellow' ? 'missile' : '');
-}
+  return "block " + (props.color === "yellow" ? "missile" : "");
+};
 
 const BlockComponent: React.FC<BlockProps> = (props) => (
-  <div className={blockClassName(props)} style={{backgroundColor: props.color}}>
+  <div
+    className={blockClassName(props)}
+    style={{ backgroundColor: props.color }}
+  >
     {props.children}
   </div>
 );
 
-const Blocks: React.FC<BlocksProps> = (props) => (createBlocks(props.window));
+const Blocks: React.FC<BlocksProps> = (props) => createBlocks(props.window);
 
 const keyList: KeyMapping[] = [
-  { keyValue: SPACE, keySymbol: 'space'},
-  { keyValue: LEFT, keySymbol: 'left' },
-  { keyValue: UP, keySymbol: 'up' },
-  { keyValue: RIGHT, keySymbol: 'right' },
-  { keyValue: DOWN, keySymbol: 'down' }
+  { keyValue: SPACE, keySymbol: "space" },
+  { keyValue: LEFT, keySymbol: "left" },
+  { keyValue: UP, keySymbol: "up" },
+  { keyValue: RIGHT, keySymbol: "right" },
+  { keyValue: DOWN, keySymbol: "down" },
 ];
 
 const getKeySymbol = (keyValue: number): string | null => {
-  const found = _.find(keyList, key => (key.keyValue === keyValue));
+  const found = _.find(keyList, (key) => key.keyValue === keyValue);
   return found ? found.keySymbol : null;
-}
+};
 
-class App extends Component<{}, GameState> {
-  private launchMissile: (e: { which: number }) => void;
-  private timer?: NodeJS.Timeout;
+const App: React.FC = () => {
+  const [gameState, setGameState] = useState<GameState>(() =>
+    fpBlock.init(40, 30),
+  );
+  const timerRef = useRef<NodeJS.Timeout>();
 
-  constructor(props: {}) {
-    super(props);
-    this.state = fpBlock.init(40, 30);
-    this.timer = setInterval(() => {
-      this.setState((state) => (fpBlock.tick(state)));
-    }, 150);
-
-    this.launchMissile = _.throttle((e: { which: number }) => {
-      this.setState((state) => {
+  const launchMissile = useCallback(
+    _.throttle((e: { which: number }) => {
+      setGameState((state) => {
         const symbol = getKeySymbol(e.which);
         return symbol ? fpBlock.key(symbol, state) : state;
       });
-    }, 500);
+    }, 500),
+    [],
+  );
 
-    keyboard.keyPressed((e: { which: number }) => {
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setGameState((state) => fpBlock.tick(state));
+    }, 150);
+
+    const handleKeyPress = (e: { which: number }) => {
       if (e.which === UP) {
-        this.launchMissile(e);
+        launchMissile(e);
       } else if (e.which === KEY_L) {
-        this.setState((state) => (state.savedState || state));
+        setGameState((state) => state.savedState || state);
       } else if (e.which === KEY_S) {
-        this.setState((state) => ({...state, savedState: _.cloneDeep(state)}));
+        setGameState((state) => ({
+          ...state,
+          savedState: _.cloneDeep(state),
+        }));
       } else {
         setTimeout(() => {
-          this.setState((state) => {
+          setGameState((state) => {
             const symbol = getKeySymbol(e.which);
             return symbol ? fpBlock.key(symbol, state) : state;
           });
         });
       }
-    });
-  }
+    };
 
-  render(): React.ReactElement {
-    return (
-      <div className="container">
-        <div className="App">
-          <Blocks window={_.flatten(fpBlock.join(this.state))} />
-        </div>
+    keyboard.keyPressed(handleKeyPress);
+
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
+  }, [launchMissile]);
+
+  return (
+    <div className="container">
+      <div className="App">
+        <Blocks window={_.flatten(fpBlock.join(gameState))} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
